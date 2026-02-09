0d82482ca1c94b58b96afb4254ae3a1a79dca880
Ferdi <129670973+Forsfer@users.noreply.github.com>
2026-02-09 08:14:04 +0100

    Setup for create tables in staging
    This script creates tables in the staging schema, dropping existing tables if they already exist.

---
+++ SQL/staging/01_create_tables.sql
@@ -0,0 +1,9 @@
+/*
+===============================================================================
+Staging Script: Create Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the staging schema, dropping existing tables 
+    if they already exist.
+===============================================================================
+*/
